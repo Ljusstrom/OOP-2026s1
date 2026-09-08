d9392651318eecdaeb11a9d1bda951f7a2d7e228
a308 <matti.valovirta@metropolia.fi>
2026-09-08 10:09:11 +0300

    Added live code for module 5.1

--- Moduuli6_1/src/paketti/Luokka.java
+++ Moduuli6_1/src/paketti/Luokka.java
@@ -16,4 +16,14 @@ public class Luokka {
         }
     }
 
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 4;
+        int c = a + b;
+
+        Luokka l = new Luokka();
+        l.getTieto();
+
+    }
+
 }
